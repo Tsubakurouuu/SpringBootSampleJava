14622ec388beef2268897cb32f7ffd40ca512a1d
Tsubakurouuu <yzpolice2896@icloud.com>
2023-02-20 16:08:11 +0900

    Spring Freamwork超入門 ~やさしくわかるWEBアプリ開発~ 3章 3-1

---
+++ src/main/java/com/example/SpringBootSampleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleJavaApplication.class, args);
+	}
+
+}
